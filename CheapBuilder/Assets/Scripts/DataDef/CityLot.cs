37b7b2522f0fd21915a4d216639e9c6a9cc664c4
Brian Anderson <whynotcoderandwhy@gmail.com>
2020-02-01 12:00:33 -0500

    Small continuation of basics defintions

---
+++ CheapBuilder/Assets/Scripts/DataDef/CityLot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CityLot
+{
+    protected Vector2 m_location;
+    protected Vector2 m_size;
+    protected Building m_building; 
+}
+
--- CheapBuilder/Assets/Scripts/DataDef/WorkOrder.cs
+++ CheapBuilder/Assets/Scripts/DataDef/WorkOrder.cs
@@ -10,7 +10,8 @@ public class WorkOrder
     protected List<ProductOrder> m_actualMaterialList;
     protected float m_manHours;
     protected float m_spentManHours;
-    protected int m_DueDate;
+    protected int m_dueDate;
+    protected int m_startDate;
 
     /// <summary>
     /// Lets the player predict impact aside from worker factor
@@ -21,4 +22,28 @@ public class WorkOrder
         throw new System.NotImplementedException();
     }
 
+    public bool SplitActualList(int actualMaterialIndex)
+    {
+        return false;
+    }
+
+    public void LockQuantity(int actualMaterialIndex)
+    {
+
+    }
+
+    /// <summary>
+    /// Quantity of an item can never be less than one
+    /// </summary>
+    /// <param name="actualMaterialIndex"></param>
+    /// <param name="newMaterialCount"></param>
+    public void UpdateQuantity(int actualMaterialIndex, int newMaterialCount)
+    {
+
+    }
+
+    public bool RemoveActualMaterial(int acutalMaterialIndex)
+    {
+        return false;
+    }
 }
