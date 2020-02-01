75c573c02f0e3003b9e48b30e78fe01e334e0e4d
Eric Krushelniski <erickrushelniski@gmail.com>
2020-02-01 11:58:59 -0500

    I should actually stage files, eh?

--- CheapBuilder/Assets/Scripts/ModLoading/FileLoader.cs
+++ CheapBuilder/Assets/Scripts/ModLoading/FileLoader.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
 
-public class FileLoader : MonoBehaviour
+public static class FileLoader
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+
+
+
+    public static Texture2D LoadImageFromDisk(string filePath)
     {
-        
+        Texture2D img = null;
+        byte[] imgData;
+
+
+        if (File.Exists(filePath))
+        {
+            imgData = File.ReadAllBytes(filePath);
+            img = new Texture2D(2, 2);
+            if (!img.LoadImage(imgData))
+                return null;
+        }
+        return img;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- CheapBuilder/Assets/Scripts/ModLoading/HouseImageTest.cs
+++ CheapBuilder/Assets/Scripts/ModLoading/HouseImageTest.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HouseImageTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        RawImage img = gameObject.GetComponent<RawImage>();
+        Texture2D loadedImg = FileLoader.LoadImageFromDisk("C:\\Users\\krus0021\\Desktop\\CheapBuilder\\cheapBuilder\\CheapBuilder\\Resources\\Images\\House_Small.png");
+        img.texture = loadedImg;
     }
 
     // Update is called once per frame
