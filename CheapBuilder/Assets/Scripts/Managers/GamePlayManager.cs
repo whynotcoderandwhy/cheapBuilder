e1f67879ab5b9583a5042d74a448306c6b29d783
Brian <whynotcoderandwhy@gmail.com>
2020-02-02 10:25:45 -0500

    Working on quick setup for failed jobs

--- CheapBuilder/Assets/Scripts/Managers/GamePlayManager.cs
+++ CheapBuilder/Assets/Scripts/Managers/GamePlayManager.cs
@@ -13,14 +13,34 @@ public class GamePlayManager
         GameState.CurrentCash -= m_workerManager.PayWorkers();
         GameState.CurrentCash -= GameState.ActiveJobs.Select(
             ActiveJob => ActiveJob.ResolveDayAndCalculateCost()).Sum();
+        ResolveExpiredJobs();
     }
+
+    public void ResolveExpiredJobs()
+    {
+        for(int i = GameState.ActiveJobs.Count -1; i >= 0; i--)
+        {
+            if (!GameState.ActiveJobs[i].WorkComplete)
+                continue;
+            ActiveJob jobCleanup = GameState.ActiveJobs[i];
+            GameState.ActiveJobs.RemoveAt(i);
+
+            if (jobCleanup.WorkFailed)
+            {
+                //TODO: do more than just this
+                jobCleanup.ResetProgress();
+                continue;
+            }
+            GameState.CurrentCash += (1 - GameState.AdvancePercentage) * jobCleanup.JobWorth;
+        }
+    }
+
     public void RecruitWorkers() { }
 
     public void AcceptNewJob(Building JobRequester) { }
 
     public void ResolveWorkOrder()
     {
-
-
+        
     }
 }
