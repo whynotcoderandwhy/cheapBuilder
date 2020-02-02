f8e980d29715ec6efb2ce97113f52c4cf5458a26
Eric Krushelniski <erickrushelniski@gmail.com>
2020-02-02 13:15:15 -0500

    initial camera stuff

---
+++ CheapBuilder/Assets/Scripts/Camera/OrbitPoint.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class OrbitPoint
+{
+    public OrbitPoint(float yawRotationAroundPivit, float pitchRotationAroundPivit, float distanceFromPivot, float maxPitch, float minPitch, float maxYaw, float minYaw)
+    {
+        m_yawRotationAroundPivit = yawRotationAroundPivit;
+        m_pitchRotationAroundPivit = pitchRotationAroundPivit;
+        m_distanceFromPivot = distanceFromPivot;
+        m_maxPitch = maxPitch;
+        m_minPitch = minPitch;
+        m_maxYaw = maxYaw;
+        m_minYaw = minYaw;
+    }
+
+    [SerializeField]
+    protected float m_yawRotationAroundPivit;
+    [SerializeField]
+    protected float m_pitchRotationAroundPivit;
+    [SerializeField]
+    protected float m_distanceFromPivot;
+    [SerializeField]
+    protected float m_maxPitch;
+    [SerializeField]
+    protected float m_minPitch;
+    [SerializeField]
+    protected float m_maxYaw;
+    [SerializeField]
+    protected float m_minYaw;
+
+    public void Increment(float yaw, float pitch)
+    {
+        m_yawRotationAroundPivit %= 360;
+
+        m_yawRotationAroundPivit = Mathf.Clamp(m_yawRotationAroundPivit + yaw, m_minYaw, m_maxYaw);
+        m_pitchRotationAroundPivit = Mathf.Clamp(m_pitchRotationAroundPivit + pitch, m_minPitch, m_maxPitch);
+    }
+
+    /// <summary>
+    /// the purpose of return target point it to calcuate the desired location of an object based on a rotation given a pitch and yaw and a distance
+    /// </summary>
+    /// <returns>returns a direction from a point in the rotation which is a unit vector distance * distance away  </returns>
+    public Vector3 ReturnTargetPoint()
+    {
+        return Quaternion.Euler(m_pitchRotationAroundPivit, m_yawRotationAroundPivit, 0.0f) * Vector3.forward * m_distanceFromPivot;
+    }
+}
---
+++ CheapBuilder/Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public KeyCode forwards = KeyCode.W;
+    public KeyCode backwards = KeyCode.S;
+    public KeyCode left = KeyCode.A;
+    public KeyCode right = KeyCode.D;
+    public KeyCode up = KeyCode.Q;
+    public KeyCode down = KeyCode.E;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(forwards))
+            gameObject.transform.position += Vector3.forward * 5.0f;
+        if (Input.GetKeyDown(backwards))
+            gameObject.transform.position -= Vector3.forward * 5.0f;
+        if (Input.GetKeyDown(left))
+            gameObject.transform.position -= Vector3.right * 5.0f;
+        if (Input.GetKeyDown(right))
+            gameObject.transform.position += Vector3.right * 5.0f;
+        if (Input.GetKeyDown(up))
+            gameObject.transform.position += Vector3.up * 5.0f;
+        if (Input.GetKeyDown(down))
+            gameObject.transform.position -= Vector3.up * 5.0f;
+
+
+
+    }
+}
